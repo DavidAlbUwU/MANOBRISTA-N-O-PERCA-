7334a1be69a83e365c922b3de859fde5b5c5f8fd
DavidAlbUwU <117684736+DavidAlbUwU@users.noreply.github.com>
2023-03-15 17:58:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+greencar_width = 75;
+greencar_height = 100; 
+
+background_image = "parking.png";
+greencar_image = "car2.png";
+
+greencar_X = 5;
+greencar_Y = 225
+
+function add() {
+	background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+    greencar_imgTag = new Image();
+    greencar_imgTag.onload = uploadGreenCar;
+    greencar_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+
+}
+
+function uploadGreenCar() {
+	ctx.drawImage(greencar_imgTag,greencar_X,greencar_Y,greencar_width,greencar_height);
+
+	
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(greencar_Y >=0)
+    {
+        greencar_Y = greencar_Y - 10;
+        console.log("Quando direcional cima for pressionada,  x = " + greencar_X + " | y = " +greencar_Y);
+         uploadBackground();
+         uploadGreenCar();
+    }
+}
+
+function down()
+{
+	if(greencar_Y <=350)
+    {
+        greencar_Y = greencar_Y + 10;
+        console.log("Quando direcional baixo for pressionada,  x = " + greencar_X + " | y = " +greencar_Y);
+         uploadBackground();
+         uploadGreenCar();
+    }
+}
+
+function left()
+{
+	if(greencar_X >=0)
+    {
+        greencar_X = greencar_X - 10;
+        console.log("Quando direcional esquerda for pressionada,  x = " + greencar_X + " | y = " +greencar_Y);
+         uploadBackground();
+         uploadGreenCar();
+    }
+}
+
+function right()
+{
+	if(greencar_X <=750)
+    {
+        greencar_X = greencar_X + 10;
+        console.log("Quando direcional direita for pressionada,  x = " + greencar_X + " | y = " +greencar_Y);
+         uploadBackground();
+         uploadGreenCar();
+    }
+}
